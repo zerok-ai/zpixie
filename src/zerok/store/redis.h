1002a30fca9e9f7538f55a2551c177233a1e0eee
Avin Goyal <avingoyal01@gmail.com>
2023-05-12 11:45:35 +0530

    adding cpp_redis

--- src/zerok/store/redis.h
+++ src/zerok/store/redis.h
@@ -1,18 +1,18 @@
-// #include "cpp_redis/cpp_redis"
-#define REDISCPP_HEADER_ONLY
-#include "redis-cpp/stream.h"
-#include "redis-cpp/execute.h"
-#include <iostream>
+#include "cpp_redis/cpp_redis"
+// #define REDISCPP_HEADER_ONLY
+// #include "redis-cpp/stream.h"
+// #include "redis-cpp/execute.h"
+// #include <iostream>
 
 namespace zk {
     class ZkStore{
         public:
             void connect(){
-                auto stream = rediscpp::make_stream("localhost", "6379");
-                auto response = rediscpp::execute(*stream, "ping");
-                std::cout << response.as<std::string>() << std::endl;
-                // cpp_redis::client client;
-                // client.connect("127.0.0.1", 6379);
+                // auto stream = rediscpp::make_stream("localhost", "6379");
+                // auto response = rediscpp::execute(*stream, "ping");
+                // std::cout << response.as<std::string>() << std::endl;
+                cpp_redis::client client;
+                client.connect("127.0.0.1", 6379);
             }
     };
 }
