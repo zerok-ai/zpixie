4297debdcd7ebbb568434f32009ba768bd6b529c
avingoyal <avingoyal01+github@gmail.com>
2023-11-17 14:12:02 +0530

    fix

--- src/zerok/store/store.h
+++ src/zerok/store/store.h
@@ -52,7 +52,7 @@ class ZkRedis : public ZkStore {
     strcpy(endpoints[0].host, host.c_str());
     endpoints[0].port = zkRedisConfig.getPort();
 
-    printf("zk-log/stores Connecting with redis " + host + "::" + zkRedisConfig.getPort() + "\n");
+    printf("zk-log/stores Connecting with redis " + host + "::%d\n", zkRedisConfig.getPort());
 
     REDIS_CONFIG conf = {
         (REDIS_ENDPOINT*)&endpoints, 1, 500, 500, 20, 1,
